8d6381f2434de1c0967cc71bf53e8c62bbacfe87
Andrew Willis <arwillis@uncc.edu>
2024-03-03 22:46:19 -0500

    Position controller based on the code from https://github.com/brianwade1/quadcopter_with_PID_controller

--- examples/altitude_controller.py
+++ examples/altitude_controller.py
@@ -44,26 +44,37 @@ class Quadcopter():
     def __init__(self, mass):
         self.num_motors = 4  # number of motors on the vehicle
         self.gravity = 9.8  # acceleration due to gravity, m/s^2
-        self.kt = 1e-7  # constant to convert motor rotational speed into thrust (T=kt*omega^2), N/(rpm)^2
-        self.kt = mass * self.gravity / (self.num_motors * 795)
-        self.b_prop = 1e-9  # constant to convert motor speed to torque (torque = b*omega^2), (N*m)/(rpm)^2
-        self.density = 1.225  # air density, kg/m^3
+        # self.kt = 1e-7  # constant to convert motor rotational speed into thrust (T=kt*omega^2), N/(rpm)^2
+        # self.kt = mass * self.gravity / (self.num_motors * 800)
+        self.kt = 5.84e-06
+        #self.b_prop = 1e-9  # constant to convert motor speed to torque (torque = b*omega^2), (N*m)/(rpm)^2
+        # self.b_prop = 0.06
+        self.b_prop = 1e-6 * 0.000175  # motor constant * rotor drag coefficient
+        # Air density at 25 deg C is about 1.1839 kg/m^3 At 20 °C and 101.325 kPa, dry air has density 1.2041 kg/m3
+        # self.density = 1.225  # air density, kg/m^3
+        self.density = 1.19  # air density, kg/m^3
         self.mass = mass  # total mass of the vehicle, kg
-        self.Ixx = 8.11858e-5  # mass-moment of inertial about x-axis, kg-m^2
-        self.Iyy = 8.11858e-5  # mass-moment of inertial about y-axis, kg-m^2
-        self.Izz = 6.12233e-5  # mass-moment of inertial about z-axis, kg-m^2
+        # self.Ixx = 8.11858e-5  # mass-moment of inertial about x-axis, kg-m^2
+        # self.Iyy = 8.11858e-5  # mass-moment of inertial about y-axis, kg-m^2
+        # self.Izz = 6.12233e-5  # mass-moment of inertial about z-axis, kg-m^2
+        self.Ixx = 0.029125  # mass-moment of inertial about x-axis, kg-m^2
+        self.Iyy = 0.029125  # mass-moment of inertial about y-axis, kg-m^2
+        self.Izz = 0.055225  # mass-moment of inertial about z-axis, kg-m^2
         self.A_ref = 0.02  # reference area for drag calcs, m^2
-        self.L = 0.2  # length from body center to prop center, m
+        # self.L = 0.2  # length from body center to prop center, m
+        self.L = 0.25554  # length from body center to prop center, m
         self.Cd = 1  # drag coefficient
         self.thrust = mass * self.gravity
         # initial speeds of motors
         # self.speeds = np.ones(self.num_motors * ((mass * self.gravity) / (self.kt * self.num_motors)))
         self.speeds = np.zeros((4, 1))
         self.tau = np.zeros(3)
-
-        self.maxT = 16.5  # max thrust from any single motor, N
-        self.minT = .5  # min thrust from any single motor, N
-        self.max_angle = np.pi / 12  # radians, max angle allowed at any time step
+        self.MAX_RPM = 1100
+        self.MIN_RPM = 0
+        # self.maxT = 16.5  # max thrust from any single motor, N
+        # self.minT = .5  # min thrust from any single motor, N
+        # self.max_angle = np.pi / 12  # radians, max angle allowed at any time step
+        self.max_angle = np.pi / 36  # radians, max angle allowed at any time step
 
         self.I = np.array([[self.Ixx, 0, 0], [0, self.Iyy, 0], [0, 0, self.Izz]])
         self.g = np.array([0, 0, -self.gravity])
@@ -129,8 +140,13 @@ if __name__ == '__main__':
 
     verbose = True
     render = True
-    current_altitude = 487.9
+
     SDF_SLOWDOWN_PARAM = 10
+    SIM_DURATION = 60
+    YAW_SETPOINT = 0 # np.pi / 2
+
+    position_xyz_sp = np.array([10, 10, 1.7])
+
     # Need to set the aircraft model after we create the environment because
     # the model is unique and can't be hardcoded in the gym init.
     # env = gym.make(args.gym_id)
@@ -148,38 +164,41 @@ if __name__ == '__main__':
     if render:
         env.render()
     env.verbose = verbose
+    env.max_sim_time = SIM_DURATION
 
     # 3DR Iris Vehicle
-    pid = SimplePID(kp=1e-4, ki=0.000000, kd=1e-2)
 
     # Gains for position controller
     # Kp_pos = [.95, .95, 15.]  # proportional [x,y,z]
     # Kd_pos = [1.8, 1.8, 15.]  # derivative [x,y,z]
     # Ki_pos = [0.2, 0.2, 1.0]  # integral [x,y,z]
-    Kp_pos = [.95, .95, 15.]  # proportional [x,y,z]
+    Kp_pos = [0.5, 0.5, 0.5]  # proportional [x,y,z]
     Kd_pos = [0, 0, 0]  # derivative [x,y,z]
-    Ki_pos = [0, 0, 0]  # integral [x,y,z]
+    Ki_pos = [0.25, 0.25, 0.25]  # integral [x,y,z]
     Ki_sat_pos = np.tile(np.array([-1.1, 1.1]), (3, 1))  # saturation for integral controller (prevent windup) [x,y,z]
 
     # Gains for angle controller
-    Kp_ang = [6.9, 6.9, 25.]  # proportional [x,y,z]
+    # Kp_ang = [6.9, 6.9, 25.]  # proportional [x,y,z]
+    # Kd_ang = [3.7, 3.7, 9.]  # derivative [x,y,z]
+    # Ki_ang = [0.1, 0.1, 0.1]  # integral [x,y,z]
+    Kp_ang = [4.0e-2, 4.0e-2, 12.0e-6]  # proportional [x,y,z]
+    # Kp_ang = [1.0, 1.0, 1.0]  # proportional [x,y,z]
     Kd_ang = [0, 0, 0]  # derivative [x,y,z]
-    Ki_ang = [0, 0, 0]  # integral [x,y,z]
+    Ki_ang = [12.0e-3, 12.0e-3, 0.0e-7]  # integral [x,y,z]
     Ki_sat_ang = np.tile(np.array([[-0.1, 0.1]]), (3, 1))  # saturation for integral controller (prevent windup) [x,y,z]
 
     position_xyz_controller = SimplePID(Kp_pos, Kd_pos, Ki_pos, Ki_sat_pos)
     orientation_rpy_controller = SimplePID(Kp_ang, Kd_ang, Ki_ang, Ki_sat_ang)
 
-    motor_rpm_sp_list = [790] * 4
+    motor_rpm_sp = [790] * 4
     #  NF 1 Vehicle
     # pid = SimplePID(kp=1e-6, ki=0.000000, kd=1e-10)
     # motor_rpm_sp = 850
 
-    pid.output_limits = (-1000, 1000)
-
     # let the quad settle onto the ground
-    while env.sim_time < 4:
-        ob, reward, done, _ = env.step_basic(motor_rpm_sp_list)
+    while env.sim_time < 2:
+        ob, reward, done, _ = env.step_basic(motor_rpm_sp)
+
     utm_zone = int((np.floor((ob.gt_wgs84_pos[1] + 180) / 6) % 60) + 1)
     proj_wgs842utm = pyproj.Proj("+proj=utm +zone={} +datum=WGS84 +units=m +no_defs".format(utm_zone))
 
@@ -188,45 +207,44 @@ if __name__ == '__main__':
 
     current_step = 0
 
-    elevation_set_point = current_altitude + 4.7
-
     orientation_rpy_initial = Rotation.from_quat(ob.gt_attitude_quat).as_euler("xyz")
-    orientation_rpy_current = orientation_rpy_initial
-    orientation_rpy_previous = orientation_rpy_current
+    orientation_rpy_ob = orientation_rpy_initial
+    orientation_rpy_previous = orientation_rpy_ob
 
     position_xyz_initial = utm_current - utm_initial
-    position_xyz_current = position_xyz_initial
-    position_xyz_previous = position_xyz_current
+    position_xyz_ob = position_xyz_initial
+    position_xyz_previous = position_xyz_ob
 
-    position_xyz_sp = np.array([0, 0, 4.7])
     orientation_rpy_sp = np.array([0, 0, 0])
     velocity_xyz_sp = np.array([0, 0, 0])
     velocity_rpy_sp = np.array([0, 0, 0])
 
     Quadcopter = Quadcopter(mass=1.5)
 
+
     # spin up the motors
-    while env.sim_time < 60:
+    while env.sim_time < SIM_DURATION:
         # error = elevation_set_point - current_altitude
         # delta_motor_rpm_sp = pid.update(env.sim_time, error)
         # delta_motor_rpm_sp = pid(current_altitude, env.stepsize)
         # motor_rpm_sp_list = [motor_rpm_sp + delta_motor_rpm_sp[0] for motor_rpm_sp in motor_rpm_sp_list]
-        ob, reward, done, _ = env.step_basic(motor_rpm_sp_list)
+        ob, reward, done, _ = env.step_basic(motor_rpm_sp)
 
-        motor_rpm_actual = SDF_SLOWDOWN_PARAM * np.array(ob.esc_motor_angular_velocity[0:4])
+        motor_rpm_ob = SDF_SLOWDOWN_PARAM * np.array(ob.esc_motor_angular_velocity[0:4])
 
         utm_current = np.array([*proj_wgs842utm(ob.gt_wgs84_pos[1], ob.gt_wgs84_pos[0]), ob.gt_wgs84_pos[2]])
-        position_xyz_current = utm_current - utm_initial
-        orientation_rpy_current = Rotation.from_quat(ob.gt_attitude_quat).as_euler("xyz")
+        position_xyz_ob = utm_current - utm_initial
+        # zyx returns yaw, pitch, roll so assign the rpy vector in reverse order
+        orientation_rpy_ob[::-1] = Rotation.from_quat(ob.gt_attitude_quat).as_euler("zyx", degrees=False)
 
-        velocity_xyz_current = (position_xyz_current - position_xyz_previous) / env.stepsize
-        velocity_rpy_current = (orientation_rpy_current - orientation_rpy_previous) / env.stepsize
+        velocity_xyz_ob = (position_xyz_ob - position_xyz_previous) / env.stepsize
+        velocity_rpy_ob = (orientation_rpy_ob - orientation_rpy_previous) / env.stepsize
 
-        position_xyz_error = position_xyz_sp - position_xyz_current
+        position_xyz_error = position_xyz_sp - position_xyz_ob
         # velocity_xyz_error = velocity_xyz_sp - velocity_xyz_current
 
         #  Calculate Quadcopter dynamics
-        thrust = Quadcopter.kt * np.sum(motor_rpm_actual)
+        thrust = Quadcopter.kt * np.sum(motor_rpm_ob)
         R_b2i = Rotation.from_quat(ob.gt_attitude_quat).as_matrix()  # changes from body frame to inertial frame
         R_i2b = R_b2i.T  # changes from inertial frame to body frame
 
@@ -234,53 +252,51 @@ if __name__ == '__main__':
         # find_lin_acc()
         thrust_body = np.array([0, 0, thrust])
         thrust_inertial = R_b2i @ thrust_body
-        velocity_body = R_i2b @ velocity_xyz_current
+        velocity_body = R_i2b @ velocity_xyz_ob
         drag_body = -Quadcopter.Cd * 0.5 * Quadcopter.density * Quadcopter.A_ref * velocity_body ** 2
         drag_inertial = R_b2i @ drag_body
-        weight = Quadcopter.mass * Quadcopter.g
-        acceleration_inertial = (thrust_inertial + drag_inertial + weight) / Quadcopter.mass
+        # weight = Quadcopter.mass * Quadcopter.g
+        # acceleration_xyz_ob = (thrust_inertial + drag_inertial + weight) / Quadcopter.mass
 
         # torque on body from motor thrust differential
         # find_body_torque()
         L = Quadcopter.L
         kt = Quadcopter.kt
         b_prop = Quadcopter.b_prop
-        tau = np.array([(L * kt * (motor_rpm_actual[3] - motor_rpm_actual[1])),
-                        (L * kt * (motor_rpm_actual[2] - motor_rpm_actual[0])),
-                        (b_prop * (-motor_rpm_actual[0] + motor_rpm_actual[1] -
-                                   motor_rpm_actual[2] + motor_rpm_actual[3]))])
+
+        tau_ob = np.array([(L * kt * (-motor_rpm_ob[0] + motor_rpm_ob[1] + motor_rpm_ob[2] - motor_rpm_ob[3])),
+                           (L * kt * (-motor_rpm_ob[0] + motor_rpm_ob[1] - motor_rpm_ob[2] + motor_rpm_ob[3])),
+                           (b_prop * (-motor_rpm_ob[0] - motor_rpm_ob[1] + motor_rpm_ob[2] + motor_rpm_ob[3]))])
 
         # Angular acceleration in inertial frame
         # omega = self.thetadot2omega()  #angles in inertial frame
         # omega_dot = self.find_omegadot(omega) #angular acceleration in inertial frame
-        R = np.array([[1, 0, -s(orientation_rpy_current[1])],
-                      [0, c(orientation_rpy_current[0]),
-                       c(orientation_rpy_current[1]) * s(orientation_rpy_current[0])],
-                      [0, -s(orientation_rpy_current[0]),
-                       c(orientation_rpy_current[1]) * c(orientation_rpy_current[0])]])
-        omega = R @ velocity_rpy_current
-        I = Quadcopter.I
-        omega_dot = np.linalg.inv(I).dot(tau - np.cross(omega, I @ omega))
+        R = np.array([[1, 0, -s(orientation_rpy_ob[1])],
+                      [0, c(orientation_rpy_ob[0]), c(orientation_rpy_ob[1]) * s(orientation_rpy_ob[0])],
+                      [0, -s(orientation_rpy_ob[0]), c(orientation_rpy_ob[1]) * c(orientation_rpy_ob[0])]])
+        omega = R @ velocity_rpy_ob
+        Imat = Quadcopter.I
+        omega_dot = np.linalg.inv(Imat).dot(tau_ob - np.cross(omega, Imat @ omega))
 
         # Angular acceleration in body frame
         # self.omegadot2Edot(omega_dot)
-        R = np.array([[1, s(orientation_rpy_current[0]) * t(orientation_rpy_current[1]),
-                       c(orientation_rpy_current[0]) * t(orientation_rpy_current[1])],
-                      [0, c(orientation_rpy_current[0]), -s(orientation_rpy_current[0])],
-                      [0, s(orientation_rpy_current[0]) / c(orientation_rpy_current[1]),
-                       c(orientation_rpy_current[0]) / c(orientation_rpy_current[1])]])
+        R = np.array([[1, s(orientation_rpy_ob[0]) * t(orientation_rpy_ob[1]),
+                       c(orientation_rpy_ob[0]) * t(orientation_rpy_ob[1])],
+                      [0, c(orientation_rpy_ob[0]), -s(orientation_rpy_ob[0])],
+                      [0, s(orientation_rpy_ob[0]) / c(orientation_rpy_ob[1]),
+                       c(orientation_rpy_ob[0]) / c(orientation_rpy_ob[1])]])
 
         E_dot = R @ omega_dot
         acceleration_rpy_current = E_dot
 
         if current_step % 1000 == 0:
-            print("step={} height_sp={:.2f} h={:.2f} ".format(current_step, elevation_set_point, current_altitude) +
-                  "set_point_rpm=({:.1f},{:.1f},{:.1f},{:.1f}) ".format(*motor_rpm_sp_list) +
-                  "actual_rpm=({:.1f},{:.1f},{:.1f},{:.12f}) ".format(*motor_rpm_actual) +
-                  # "delta_rpm_sp={:.1f} rpm ".format(delta_motor_rpm_sp[0] / env.stepsize) +
-                  "(x,y,z)=({:.2f}, {:.2f}, {:.2f})".format(*position_xyz_current) +
-                  "(r,p,y)=({:.1f}, {:.1f}, {:.1f})".format(*[arad * 180 / np.pi for arad in orientation_rpy_current]))
-            test = True
+            print("step={} ".format(current_step) +
+                  "rpm_sp=({:.1f},{:.1f},{:.1f},{:.1f}) ".format(*motor_rpm_sp) +
+                  "rpm_ob=({:.1f},{:.1f},{:.1f},{:.1f}) ".format(*motor_rpm_ob) +
+                  "(x,y,z)_sp=({:.2f}, {:.2f}, {:.2f}) ".format(*position_xyz_sp) +
+                  "(x,y,z)_ob=({:.2f}, {:.2f}, {:.2f}) ".format(*position_xyz_ob) +
+                  "(r,p,y)_sp=({:.2f}, {:.2f}, {:.2f}) ".format(*[av * 180 / np.pi for av in orientation_rpy_sp]) +
+                  "(r,p,y)_ob=({:.1f}, {:.1f}, {:.1f})".format(*[av * 180 / np.pi for av in orientation_rpy_ob]))
         # if done:
         #     break
 
@@ -288,7 +304,8 @@ if __name__ == '__main__':
         acceleration_xyz_sp = position_xyz_controller.update(env.stepsize, position_xyz_error)
 
         # Modify z gain to include thrust required to hover
-        # acceleration_xyz_sp[2] = (gravity + des_acc[2]) / (math.cos(quadcopter.angle[0]) * math.cos(quadcopter.angle[1]))
+        # acceleration_xyz_sp[2] = (Quadcopter.gravity +
+        #                            acceleration_xyz_sp[2]) / (c(orientation_rpy_ob[0]) * c(orientation_rpy_ob[1]))
 
         # calculate thrust needed
         thrust_sp = Quadcopter.mass * acceleration_xyz_sp[2]
@@ -298,18 +315,24 @@ if __name__ == '__main__':
         if acceleration_xyz_magnitude == 0:
             acceleration_xyz_magnitude = 1
 
+        # roll changes Y coordinate, pitch changes X coordinate, yaw stays at 0
         # use desired acceleration to find desired angles since the quad can only move via changing angles
+        sin_roll_angle_sp = -acceleration_xyz_sp[1] / acceleration_xyz_magnitude / np.cos(orientation_rpy_ob[1])
+        if np.abs(sin_roll_angle_sp) > 1:
+            sin_roll_angle_sp /= np.abs(sin_roll_angle_sp)
         orientation_rpy_sp = [
-            np.arcsin(-acceleration_xyz_sp[1] / acceleration_xyz_magnitude / np.cos(orientation_rpy_current[1])),
-            np.arcsin(acceleration_xyz_sp[0] / acceleration_xyz_magnitude), 0]
+            np.arcsin(sin_roll_angle_sp),
+            np.arcsin(acceleration_xyz_sp[0] / acceleration_xyz_magnitude), YAW_SETPOINT]
 
         # check if exceeds max angle
-        orientation_rpy_magnitude = np.linalg.norm(orientation_rpy_sp)
+        orientation_rpy_magnitude = np.linalg.norm(orientation_rpy_sp[:2])
         if orientation_rpy_magnitude > Quadcopter.max_angle:
-            orientation_rpy_sp = (orientation_rpy_sp / orientation_rpy_magnitude) * Quadcopter.max_angle
+            orientation_rpy_sp[:2] = (orientation_rpy_sp[:2] / orientation_rpy_magnitude) * Quadcopter.max_angle
 
         # call angle controller
-        orientation_rpy_error = orientation_rpy_sp - orientation_rpy_current
+        orientation_rpy_error = orientation_rpy_sp - orientation_rpy_ob
+        if orientation_rpy_error[2] > Quadcopter.max_angle / 10:
+            orientation_rpy_error[2] = Quadcopter.max_angle / 10
         # velocity_rpy_error = velocity_rpy_sp - velocity_rpy_current
 
         tau_sp = orientation_rpy_controller.update(env.stepsize, orientation_rpy_error)
@@ -317,7 +340,7 @@ if __name__ == '__main__':
         # Find motor speeds needed to achieve desired linear and angular accelerations
         # quadcopter.des2speeds(thrust_needed, tau_needed)
         # ''' finds speeds of motors to achieve a desired thrust and torque '''
-        # Needed torque on body
+        # Needed torque on body (e1,e2,e3) = torque on (x,y,z) axes = (roll, pitch, yaw)
         e1 = tau_sp[0] * Quadcopter.Ixx
         e2 = tau_sp[1] * Quadcopter.Iyy
         e3 = tau_sp[2] * Quadcopter.Izz
@@ -325,29 +348,23 @@ if __name__ == '__main__':
         n = Quadcopter.num_motors
         # Thrust desired converted into motor speeds
         weight_speed = thrust_sp / (n * Quadcopter.kt)
+        # Allocate thrust to the motors to achieve needed thrust and torque on body
+        motor_rpm_sp = [
+            # THRUST     # ROLL CONTROL COLUMN # PITCH CONTROL COLUMN # YAW CONTROL COLUMN
+            weight_speed - (e1 / (n * kt * L)) - (e2 / (n * kt * L)) - (e3 / (n * b_prop)),
+            weight_speed + (e1 / (n * kt * L)) + (e2 / (n * kt * L)) - (e3 / (n * b_prop)),
+            weight_speed + (e1 / (n * kt * L)) - (e2 / (n * kt * L)) + (e3 / (n * b_prop)),
+            weight_speed - (e1 / (n * kt * L)) + (e2 / (n * kt * L)) + (e3 / (n * b_prop))]
 
-        # Thrust differene in each motor to achieve needed torque on body
-        motor_speeds_rpm_sp = []
-        motor_speeds_rpm_sp.append(weight_speed - (e2 / ((n / 2) * kt * L)) - (e3 / (n * b_prop)))
-        motor_speeds_rpm_sp.append(weight_speed - (e1 / ((n / 2) * kt * L)) + (e3 / (n * b_prop)))
-        motor_speeds_rpm_sp.append(weight_speed + (e2 / ((n / 2) * kt * L)) - (e3 / (n * b_prop)))
-        motor_speeds_rpm_sp.append(weight_speed + (e1 / ((n / 2) * kt * L)) + (e3 / (n * b_prop)))
-
-        motor_speeds_rpm_sp = [_clamp(motor_speed_rpm, [0, 1000]) for motor_speed_rpm in motor_speeds_rpm_sp]
-        motor_rpm_sp_list = motor_speeds_rpm_sp
-        # Ensure that desired thrust is within overall min and max of all motors
-        thrust_all = np.array(motor_speeds_rpm_sp) * kt
+        if (np.any(np.isnan(motor_rpm_sp)) is True
+                or (max(motor_rpm_sp) - min(motor_rpm_sp)) / max(motor_rpm_sp) > 0.1):
+            test_condition = True
+        MAX_RPM = Quadcopter.MAX_RPM
+        MIN_RPM = Quadcopter.MIN_RPM
+        # Ensure that desired speed is within overall min and max of all motors
+        motor_rpm_sp = [_clamp(motor_speed_rpm, [MIN_RPM, MAX_RPM]) for motor_speed_rpm in motor_rpm_sp]
+        thrust_all = np.array(motor_rpm_sp) * kt
         total_thrust = np.sum(thrust_all)
-        # over_max = np.argwhere(thrust_all > Quadcopter.maxT)
-        # under_min = np.argwhere(thrust_all < Quadcopter.minT)
-
-        # if over_max.size != 0:
-        #     for i in range(over_max.size):
-        #         motor_speeds[over_max[i][0]] = Quadcopter.maxT / kt
-        # if under_min.size != 0:
-        #     for i in range(under_min.size):
-        #         motor_speeds[under_min[i][0]] = Quadcopter.minT /kt
-        # self.speeds = motor_speeds
 
         current_altitude = ob.gt_wgs84_pos[2]
         current_step += 1
